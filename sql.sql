8767ca0bec7555ca2b1f1b0637e89f16544fefb8
Christian Villanueva <christianvillanueva@iesebre.com>
2024-10-30 16:06:46 +0100

    treball acabat

---
+++ sql.sql
@@ -0,0 +1,11 @@
+create table phpThinkering.albums
+(
+    id     int auto_increment
+        primary key,
+    name   varchar(255) null,
+    singer varchar(255) null,
+    songs  int          null,
+    year   int          null
+);
+
+
